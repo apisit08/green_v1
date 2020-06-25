5913255b17f52ab20191a9136233ec514d2fc56f
apisit08 <apisit_melove@hotmail.com>
2020-06-25 15:38:59 +0700

    password and porfile

--- src/components/account/password.vue
+++ src/components/account/password.vue
@@ -2,26 +2,26 @@
     <v-container>
       <v-card outlined>
 
-          <v-card-title class="pt-8">Change your Password</v-card-title>
+          <v-card-title class="pt-8">เปลี่ยนรหัสผ่าน</v-card-title>
           <v-divider></v-divider>
 
           <v-card-text>
 
               <v-row>
-                  <v-col cols="12" sm="6" md="6">
-                      <v-text-field v-model="fullName" outlined dense label="Old password"></v-text-field>
+                  <v-col cols="12" sm="12" md="12">
+                      <v-text-field v-model="oldpass" outlined dense label="รหัสผ่านปัจจุบัน"></v-text-field>
                   </v-col>
               </v-row>
 
               <v-row class="mt-n2">
                   <v-col cols="12" sm="12" md="12">
-                      <v-text-field dense v-model="provider" filled outlined readonly label="New passsword"></v-text-field>
+                      <v-text-field dense v-model="newpass" type="password" outlined label="รหัสผ่านใหม่"></v-text-field>
                   </v-col>
               </v-row>
 
               <v-row class="mt-n3">
                   <v-col cols="12" sm="12" md="12">
-                      <v-overflow-btn editable v-model="language" label="New password confirmation" :items="dropdown_edit" item-value="text" class="my-2"></v-overflow-btn>
+                      <v-text-field dense v-model="confirmpass" type="password" :rules="rules" outlined label="ยืนยันรหัสผ่านใหม่"></v-text-field>
                   </v-col>
               </v-row>
 
@@ -29,7 +29,7 @@
 
           <v-card-actions>
               <v-spacer></v-spacer>
-            <v-btn color="orange" style="color:white;">แก้ไข</v-btn>
+            <v-btn color="primary" style="color:white;">update</v-btn>
           </v-card-actions>
 
       </v-card>
@@ -40,17 +40,16 @@
 export default {
   data () {
     return {
-      fullname: null,
-      email: null,
-      provider: null,
-      language: null,
-      image: null,
-      imageShow: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
-      dropdown_edit: [
-        { text: 'ไทย' },
-        { text: 'อังกฤษ' }
+      oldpass: null,
+      newpass: null,
+      confirmpass: null,
+      rules: [
+        value => !!value || 'Required.',
+        value => (value === this.newpass) || 'รหัสผ่านไม่ตรงกัน'
       ]
     }
+  },
+  methods: {
   }
 }
 </script>
---
+++ src/components/account/profiles.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-container>
+      <v-card outlined>
+
+          <v-card-title class="pt-8">ลบบัญชีของคุณอย่างถาวร</v-card-title>
+          <v-divider></v-divider>
+
+          <v-card-text>
+
+              <v-row>
+                  <v-col cols="12" sm="12" md="12" class="text-center">
+                      <label>ถ้าคุณลบบัญชีของคุณ, คุณจะ <b>ไม่</b>สามารถกู้คืนบัญชีของคุณได้อีก, รวมไปถึงการตั้งค่าต่างๆ และประวัติห้องสนทนาจะถูกลบ </label>
+                  </v-col>
+              </v-row>
+
+              <v-row class="mt-n2">
+                  <v-col cols="12" sm="12" md="12" class="text-center">
+                        <v-btn color="red" style="color:white;">ใช่ ฉันต้องการลบบัญชีของฉัน</v-btn>
+                  </v-col>
+              </v-row>
+          </v-card-text>
+      </v-card>
+    </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      fullname: null,
+      email: null,
+      provider: null,
+      language: null,
+      image: null,
+      imageShow: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
+      dropdown_edit: [
+        { text: 'ไทย' },
+        { text: 'อังกฤษ' }
+      ]
+    }
+  }
+}
+</script>
