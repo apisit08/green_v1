9a1d76dc825944a52e4235c6064129291f82203c
metinie <55970200+metinie@users.noreply.github.com>
2020-07-02 17:27:19 +0700

    user update

--- src/components/account/accountInfo.vue
+++ src/components/account/accountInfo.vue
@@ -9,34 +9,43 @@
 
               <v-row>
                   <v-col cols="12" sm="6" md="6">
-                      <v-text-field v-model="fullName" outlined dense label="ชื่อ - นามสกุล"></v-text-field>
+                      <v-text-field v-model="name" outlined dense label="Name"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="6">
-                      <v-text-field v-model="email" outlined dense label="อีเมล"></v-text-field>
+                      <v-text-field v-model="phonenumber" outlined dense label="Phone number"></v-text-field>
                   </v-col>
               </v-row>
 
-              <v-row class="mt-n2">
-                  <v-col cols="12" sm="12" md="12">
+              <v-row>
+                <v-col cols="12" sm="6" md="6">
+                      <v-text-field v-model="email" outlined dense label="Email" readonly filled></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="6" class="mt-n2">
                       <v-text-field dense v-model="provider" filled outlined readonly label="ผู้ให้บริการ"></v-text-field>
                   </v-col>
               </v-row>
 
-              <v-row class="mt-n3">
-                  <v-col cols="12" sm="12" md="12">
+              <v-row>
+                  <v-col cols="12" sm="12" md="12" class="mt-n3">
                       <v-overflow-btn editable v-model="language" label="ภาษา" :items="dropdown_edit" item-value="text" class="my-2"></v-overflow-btn>
                   </v-col>
               </v-row>
 
-              <v-row class="mt-n2">
+              <v-row>
+                <span style="color: black;" class="ml-4 mb-4">รูปภาพ</span>
+              </v-row>
+
+              <v-row>
                   <v-col cols="12" sm="2" md="2">
-                      <div style="color: black;" class="ml-4 mb-4">รูปภาพ</div>
                       <v-avatar class="profile" size="100" style="border-radius: 10px;">
-                        <v-img :src="imageShow" class="ml-6" v-model="imageShow"></v-img>
+                        <v-img :src="avatar_profile" v-model="avatar_profile"></v-img>
                       </v-avatar>
                   </v-col>
-                  <v-col cols="12" sm="10" md="10">
-                      <v-text-field dense v-model="imageShow" outlined style="margin-top:65px;" placeholder="URL ภาพโปรไฟล์"></v-text-field>
+                  <v-col cols="12" sm="7" md="7">
+                      <v-text-field dense v-model="avatar_profile" outlined style="margin-top:65px;" placeholder="URL ภาพโปรไฟล์"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="3" md="3">
+                    <v-btn color="success" style="margin-top: 66px; margin-left: 35px;" @click="uploadPhoto">Update Avatar</v-btn>
                   </v-col>
               </v-row>
 
@@ -48,7 +57,7 @@
             <v-btn color="orange" style="color:white;" @click="alertConfirmed">แก้ไข</v-btn>
           </v-card-actions>
 
-          <ConfirmedUpdateAccountInfo ref="popupConfirmed" />
+          <!-- <ConfirmedUpdateAccountInfo ref="popupConfirmed" /> -->
 
       </v-card>
     </v-container>
@@ -63,11 +72,14 @@ export default {
   },
   data () {
     return {
-      fullName: 'John Leider',
-      email: 'john.leider@gmail.com',
+      user: [],
+      id: null,
+      name: null,
+      phonenumber: null,
+      email: null,
       provider: 'INET',
       language: null,
-      imageShow: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
+      avatar_profile: null,
       dropdown_edit: [
         { text: 'ไทย' },
         { text: 'อังกฤษ' }
@@ -75,11 +87,78 @@ export default {
     }
   },
   methods: {
+    uploadPhoto () {
+      var data = {
+        avatar_profile: this.avatar_profile === '' ? 'https://p7.hiclipart.com/preview/518/320/1007/computer-icons-mobile-app-development-android-my-account-icon.jpg' : this.avatar_profile
+      }
+      // http://localhost:9213' + '/api/avatar/pictureprofile'
+      this.axios.post('http://localhost:9213' + '/api/avatar/pictureprofile', data).then((response) => {
+        console.log(response.data)
+        if (response.data.status === 'register success') {
+          this.$swal('Register successfull.', '', 'success')
+          // this.name = ''
+          // this.email = ''
+          // this.password = ''
+          // this.confirmedPassword = ''
+          // this.phonenumber = ''
+          // this.oneid = ''
+          // this.avatar_profile = ''
+        } else {
+          this.$swal('ERROR !', 'Please, try again', 'error')
+          // this.name = ''
+          // this.email = ''
+          // this.password = ''
+          // this.confirmedPassword = ''
+          // this.phonenumber = ''
+          // this.oneid = ''
+          // this.avatar_profile = ''
+        }
+      })
+    },
     // openModalAccountInfo () {
     //   this.$refs.popupConfirmed.openAccountInfo()
     // },
+    confirmedUpdate () {
+      var data = {
+        id: this.id,
+        name: this.name,
+        // email: this.email,
+        // password: this.password,
+        phonenumber: this.phonenumber
+      }
+      // console.log(data.id)
+      // http://localhost:9213' + '/api/users/update'
+      var token = localStorage.getItem('user-token')
+      try {
+        this.axios.put('http://localhost:9213' + '/api/users/update', data, {
+          headers: {
+            Authorization: 'bearer ' + token
+          }
+        }).then((response) => {
+          console.log(response.data)
+          if (response.data.status === 200) {
+            this.$swal('Update successfull.', '', 'success')
+          // this.name = ''
+          // this.email = ''
+          // this.password = ''
+          // this.confirmedPassword = ''
+          // this.phonenumber = ''
+          // this.oneid = ''
+          } else {
+            this.$swal('ERROR !', 'Please, try again', 'error')
+          // this.name = ''
+          // this.email = ''
+          // this.password = ''
+          // this.confirmedPassword = ''
+          // this.phonenumber = ''
+          // this.oneid = ''
+          }
+        })
+      } catch (error) {
+        console.log(error.message)
+      }
+    },
     alertConfirmed () {
-      // Use sweetalert2
       this.$swal({
         title: 'ยืนยันแก้ไขข้อมูลบัญชี ?',
         text: 'ต้องการแก้ไขข้อมูลบัญชีใช่หรือไม่',
@@ -91,14 +170,31 @@ export default {
         cancelButtonText: 'ยกเลิก'
       }).then((result) => {
         if (result.value) {
-          this.$swal(
-            'สำเร็จ !',
-            'เปลี่ยนข้อมูลบัญชีแล้ว.',
-            'success'
-          )
+          this.confirmedUpdate()
+          // this.$swal(
+          //   'สำเร็จ !',
+          //   'เปลี่ยนข้อมูลบัญชีแล้ว.',
+          //   'success'
+          // )
         }
       })
+    },
+    decodeToken () {
+      var d = localStorage.getItem('user-token')
+      var base64Url = d.split('.')[1]
+      this.datapage = JSON.parse(window.atob(base64Url))
+      // console.log('datatoken', this.datapage)
+      this.user = this.datapage.users
+      console.log('user', this.user)
+      this.id = this.user._id
+      this.name = this.user.name
+      this.phonenumber = this.user.phonenumber
+      this.email = this.user.email
+      this.avatar_profile = this.user.avatar_profile
     }
+  },
+  created () {
+    this.decodeToken()
   }
 }
 </script>
