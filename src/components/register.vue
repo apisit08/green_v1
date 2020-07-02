896fe7709e9cae7a282f853f7ba5577fafa5276e
metinie <55970200+metinie@users.noreply.github.com>
2020-07-02 17:26:56 +0700

    user register

--- src/components/register.vue
+++ src/components/register.vue
@@ -3,25 +3,42 @@
   <v-layout align-center justify-center>
       <v-flex xs12 sm8 md4>
         <v-card shaped>
-      <v-toolbar color="#2d90c2"><v-toolbar-title style="color:white;" >Register</v-toolbar-title></v-toolbar>
+      <v-toolbar color="#2d90c2"><v-toolbar-title style="color:white;" >Create an Account</v-toolbar-title></v-toolbar>
       <v-card-text>
           <v-row>
-              <v-col cols="12" sm="12" md="12">
-                <v-text-field label="ชื่อเต็ม" v-model="fullName" required solo prepend-icon="mdi-account"></v-text-field>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field label="Name" v-model="name" required outlined></v-text-field>
               </v-col>
-              <v-col cols="12" sm="12" md="12">
-                <v-text-field label="อีเมล์" v-model="email" required solo prepend-icon="mdi-email"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="12" md="12">
-                <v-text-field label="รหัสผ่าน" v-model="password" type="password" required solo prepend-icon="mdi-lock"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="12" md="12">
-                <v-text-field label="การยืนยันรหัสผ่าน" v-model="confirmedPassword" type="password" required solo prepend-icon="mdi-lock-plus"></v-text-field>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field label="Email" v-model="email" required outlined></v-text-field>
               </v-col>
           </v-row>
+            <v-row>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field label="Password" v-model="password" type="password" required outlined></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field label="Password Confirmation" v-model="confirmedPassword" :rules="rules" type="password" required outlined></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field label="Phonenumber" v-model="phonenumber" required outlined></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field label="One ID" v-model="oneid" outlined></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col cols="12" sm="12" md="12">
+                <v-text-field label="Avatar Profile" v-model="avatar_profile" outlined></v-text-field>
+              </v-col>
+            </v-row>
+
           <v-row>
             <v-col cols="12" sm="6" md="6" align="left">
-              <v-btn color="success">
+              <v-btn color="success" @click="register">
                   <v-avatar left>
                       <v-icon>mdi-checkbox-marked-circle</v-icon>
                   </v-avatar>
@@ -48,18 +65,65 @@
 export default {
   data () {
     return {
-      fullName: '',
+      name: '',
       email: '',
       password: '',
-      confirmedPassword: ''
+      confirmedPassword: '',
+      phonenumber: '',
+      oneid: '',
+      avatar_profile: '',
+      rules: [
+        value => !!value || 'Required.',
+        value => (value === this.password) || 'รหัสผ่านไม่ตรงกัน'
+      ]
     }
   },
   methods: {
     clearData () {
-      this.fullName = ''
+      this.name = ''
       this.email = ''
       this.password = ''
       this.confirmedPassword = ''
+      this.phonenumber = ''
+      this.oneid = ''
+      this.avatar_profile = ''
+    },
+    goPage (link) {
+      this.$router.push(link)
+    },
+    register () {
+      var data = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        phonenumber: this.phonenumber,
+        oneid: this.oneid,
+        avatar_profile: this.avatar_profile === '' ? 'https://p7.hiclipart.com/preview/518/320/1007/computer-icons-mobile-app-development-android-my-account-icon.jpg' : this.avatar_profile
+      }
+      // http://localhost:9213' + '/api/users/register'
+      this.axios.post('http://localhost:9213' + '/api/users/register', data).then((response) => {
+        console.log(response.data)
+        if (response.data.status === 200) {
+          this.$swal('Register successfull.', '', 'success')
+          this.name = ''
+          this.email = ''
+          this.password = ''
+          this.confirmedPassword = ''
+          this.phonenumber = ''
+          this.oneid = ''
+          this.avatar_profile = ''
+          this.goPage('/')
+        } else {
+          this.$swal('Name Invalid !', 'This name already had. Please, try again', 'error')
+          this.name = ''
+          this.email = ''
+          this.password = ''
+          this.confirmedPassword = ''
+          this.phonenumber = ''
+          this.oneid = ''
+          this.avatar_profile = ''
+        }
+      })
     }
   }
 }
