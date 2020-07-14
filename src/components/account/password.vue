ac3a099d4718fc14f295f66a0913364cbb30b2bc
metinie <55970200+metinie@users.noreply.github.com>
2020-07-14 11:33:53 +0700

    connect api change password , reload page to get new token

--- src/components/account/password.vue
+++ src/components/account/password.vue
@@ -9,7 +9,7 @@
 
               <v-row>
                   <v-col cols="12" sm="12" md="12">
-                      <v-text-field v-model="oldpass" outlined dense label="รหัสผ่านปัจจุบัน"></v-text-field>
+                      <v-text-field v-model="oldpass" type="password" outlined dense label="รหัสผ่านปัจจุบัน"></v-text-field>
                   </v-col>
               </v-row>
 
@@ -40,6 +40,8 @@
 export default {
   data () {
     return {
+      user: [],
+      id: null,
       oldpass: null,
       newpass: null,
       confirmpass: null,
@@ -50,6 +52,43 @@ export default {
     }
   },
   methods: {
+    decodeToken () {
+      var d = localStorage.getItem('user-token')
+      var base64Url = d.split('.')[1]
+      this.datapage = JSON.parse(window.atob(base64Url))
+      // console.log('datatoken', this.datapage)
+      this.user = this.datapage.users
+      this.id = this.user._id
+      // console.log('user', this.datapage.users)
+    },
+    updatePassword () {
+      var data = {
+        user_id: this.id,
+        old_password: this.oldpass,
+        new_password: this.newpass
+      }
+      // http://localhost:9213/api/users/changepassword
+      var token = localStorage.getItem('user-token')
+      try {
+        this.axios.put(process.env.VUE_APP_API + '/api/users/changepassword', data, {
+          headers: {
+            Authorization: 'bearer ' + token
+          }
+        }).then((response) => {
+          console.log(response.data)
+          if (response.data.status === 'success') {
+            this.$swal('Update Password successfull.', '', 'success')
+            localStorage.setItem('user-token', response.data.token)
+            this.decodeToken()
+            window.location.reload()
+          } else {
+            this.$swal('ERROR !', 'Please, try again', 'error')
+          }
+        })
+      } catch (error) {
+        console.log(error.message)
+      }
+    },
     btnupdate () {
       this.$swal({
         title: 'ยืนยันเปลี่ยนรหัสผ่าน?',
@@ -62,14 +101,18 @@ export default {
         cancelButtonText: 'ยกเลิก'
       }).then((result) => {
         if (result.value) {
-          this.$swal(
-            'สำเร็จ!',
-            'เปลี่ยนรหัสผ่านแล้ว.',
-            'success'
-          )
+          this.updatePassword()
+          // this.$swal(
+          //   'สำเร็จ!',
+          //   'เปลี่ยนรหัสผ่านแล้ว.',
+          //   'success'
+          // )
         }
       })
     }
+  },
+  created () {
+    this.decodeToken()
   }
 }
 </script>
